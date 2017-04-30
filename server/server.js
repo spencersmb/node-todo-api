3982f1f757f2862852cd055a921c0cf9b57c31b8
spencer <spencer.bigum@gmail.com>
2017-04-30 16:48:05 -0400

    Finished GET /Todos Route

--- server/server.js
+++ server/server.js
@@ -7,7 +7,9 @@ const {Todo} = require('./models/todos');
 const {User} = require('./models/user');
 
 const app = express();
+
 app.use(bodyParser.json());
+
 app.post('/todos', (req, res) => {
 
     var todo = new Todo({
@@ -21,6 +23,20 @@ app.post('/todos', (req, res) => {
     });
 
 });
+
+app.get('/todos', (req, res) => {
+
+    Todo.find().then((todos) => {
+
+        res.send({
+            todos
+        })
+
+    }, (e) => {
+        res.status(400).send(e);
+    })
+
+})
 
 app.listen(3001, () => {
   console.log("===============================");
