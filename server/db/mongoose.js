a75adcb746aa3856ec191b28f05d10a00263765b
spencer <spencer.bigum@gmail.com>
2017-05-01 15:59:06 -0400

    using MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 // Connect to db first - local server
 // Set MONGO URI - heroku config:set MONGODB_URI=somevalue
-mongoose.connect('mongodb://everytuesday:westwood23@ds127391.mlab.com:27391/todoapp')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/NodeTodos')
 
 module.exports = {
     mongoose
